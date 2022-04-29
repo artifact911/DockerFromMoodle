d16ca40c50e4f64904f838b3cdf74cf61ddf370a
artifact911 <artifact911@gmail.com>
2022-04-29 15:43:11 +0300

    auth done

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,30 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const { port, host, db } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 const app = express();
-const postSchema = new mongoose.Schema({
-  name: String
-});
-const Post = mongoose.model("Post", postSchema);
-
-app.get("/test", (req, res) => {
-  res.send("Our api server is working correctly");
-});
 
 const startServer = () => {
   app.listen(port, () => {
-    console.log(`Started api service on port ${port}`);
+    console.log(`Started auth service on port ${port}`);
     console.log(`Our host is ${host}`);
     console.log(`Our db ${db}`);
-
-    const silence = new Post({name: "Silence"});
-    silence.save(function(err, savedSilence) {
-      if(err) return console.log(err);
-      console.log("savedSilence with volumes!", savedSilence);
-    });
   });
 };
+
+app.get("/test", (req, res) => {
+  res.send("Our auth server is working correctly");
+});
 
 connectDb()
   .on("error", console.log)

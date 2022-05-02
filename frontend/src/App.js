b416c8f57fcfe099c02763bf071c4822b13d8820
artifact911 <artifact911@gmail.com>
2022-05-02 10:51:06 +0300

    Started front in dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> KorbenDallas
+          We are in dev
         </p>
         <a
           className="App-link"
